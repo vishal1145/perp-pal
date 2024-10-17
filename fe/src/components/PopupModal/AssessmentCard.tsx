62f5d031e78a30d878412209ae8e589c1b1380b3
vishal1145 <vishal.kumar1145@gmail.com>
2024-10-17 18:08:51 +0530

    popup change

--- fe/src/components/PopupModal/AssessmentCard.tsx
+++ fe/src/components/PopupModal/AssessmentCard.tsx
@@ -1,24 +1,21 @@
-import { text1, text2 } from '@/data/data';
+import { text2 } from '@/data/data';
 import React, { FC } from 'react'
 type AssessmentCardProps = {
-  title:string;
-  value:string;
+  title: string;
+  value: string;
 }
-const AssessmentCard:FC<AssessmentCardProps> = ({title, value}) => {
+const AssessmentCard: FC<AssessmentCardProps> = ({ title, value }) => {
   return (
-    <div 
-    className={`block  w-full  p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100`}
-  >
-     <p className={`${text2} text-center mb-1`}>
-   {value}
-    </p>
+    <div
+      className="relative w-30 group bg-gray-100 p-4 rounded-lg shadow-md cursor-pointer hover:shadow-lg transition-shadow"
+    >
+      <h3 className="text-black font-bold text-lg  text-center">
+        {value}
+      </h3>
+      <p className={`${text2} text-center`}> {title}</p>
 
-    <h5 className={`${text1} text-center`}>
-     {title}
-    </h5>
-   
+    </div>
 
-  </div>
   )
 }
 
--- fe/src/components/PopupModal/SubmitPopup.tsx
+++ fe/src/components/PopupModal/SubmitPopup.tsx
@@ -1,114 +1,206 @@
+"use client"
 import React from 'react';
-import Loader from '../Loader';
 import AssessmentCard from './AssessmentCard';
 import { logoBtnColor, text1, text2 } from '@/data/data';
+import { FaSearch, FaMicrophone } from 'react-icons/fa';
+import { useState, useRef } from 'react';
+import { useRouter } from 'next/navigation';
 
-type SubmitPopupProps = {
-    title: string;
-    message: string;
-    setIsModalOpen: (open: boolean) => void;  
-    submitBtn: string;
-    submitFn:()=>void;
-    loaderShow:boolean;
+export type SubmitPopupProps = {
+  title: string;
+  subTitle: string;
+  message: string;
+  setIsModalOpen: (open: boolean) => void;
+  submitFn: () => void;
+  loaderShow: boolean;
+  total: number;
+  atemmpt: number;
+  correct: number;
+  incorrect: number;
+};
+
+const SubmitPopup: React.FC<SubmitPopupProps> = ({ title, subTitle, message, setIsModalOpen, submitFn, loaderShow }) => {
+  const router = useRouter();
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const [searchText, setSearchText] = useState('');
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && searchText.trim() !== '') {
+      const formattedText = searchText.trim().replace(/\s+/g, '-'); // Format the text
+      router.push(`/e-paper/${formattedText}`); // Navigate to the formatted URL
+    }
   };
-  
-  const SubmitPopup: React.FC<SubmitPopupProps> = ({ title, message, setIsModalOpen, submitBtn, submitFn, loaderShow }) => {
 
-  const closeModal = ()=>{
+  const handleMicClick = () => {
+    if ('webkitSpeechRecognition' in window) {
+      const recognition = new (window as any).webkitSpeechRecognition();
+      recognition.continuous = true;
+      recognition.interimResults = true;
+      recognition.lang = 'en-US';
+
+      let timeoutId: NodeJS.Timeout;
+
+      recognition.start();
+
+      recognition.onresult = (event: any) => {
+        const results = event.results;
+        const transcript = results[results.length - 1][0].transcript; // Get the latest result
+        setSearchText(transcript);
+
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+          recognition.stop();
+        }, 2000);
+      };
+
+      recognition.onerror = (event: any) => {
+        console.error('Speech recognition error:', event.error);
+      };
+
+      recognition.onend = () => {
+        console.log('Speech recognition service disconnected');
+        clearTimeout(timeoutId);
+      };
+    } else {
+      alert('Your browser does not support speech recognition.');
+    }
+  };
+  const closeModal = () => {
     setIsModalOpen(false);
   }
-  
- 
+
+
   return (
-        <div
-          className="fixed top-0 left-0 z-[80] w-full h-full overflow-hidden bg-black bg-opacity-50"
-          role="dialog"
-          aria-labelledby="modal-label"
-          tabIndex={-1}
-        >
-          <div className="flex items-center justify-center h-full">
-            <div className="w-full flex flex-col bg-white border shadow-lg rounded-xl border m-3 sm:max-w-lg sm:w-full " >
-              <div className="flex justify-between items-center p-4 border-b ">
-                <h3
-                  id="modal-label"
-                  className="text-sm font-medium text-black"
-                >
-                  {title}
-                </h3>
+    <div
+      className="fixed top-0 left-0 z-[80] w-full h-full overflow-hidden bg-black bg-opacity-50"
+      role="dialog"
+      aria-labelledby="modal-label"
+      tabIndex={-1}
+    >
+      <div className="flex items-center justify-center h-full">
+        <div className="w-full flex flex-col bg-white border shadow-lg rounded-xl border m-3 sm:max-w-2xl sm:w-full " >
+          <div className="flex flex-col   p-4  ">
 
-                
-          
 
-                {/* <button
-                  type="button"
-                  className="p-2 rounded-full bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none"
-                  aria-label="Close"
-                  onClick={closeModal}
-                >
-                  <span className="sr-only">Close</span>
-                  <svg
-                    className="w-4 h-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={24}
-                    height={24}
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M18 6L6 18" />
-                    <path d="M6 6l12 12" />
-                  </svg>
-                </button> */}
+            <div className='flex justify-between w-full p-0 m-0'>
+              <div
+                id="modal-label"
+                className="text-sm font-medium text-black"
+              >
+                Assessment Score
               </div>
-              <div className="p-4">
 
-<div className='flex justify-between  gap-4  '>
-  <div  style={{width:"20%"}}>
-  <AssessmentCard title='Total' value='20' />
-  </div>
 
-  <div  style={{width:"35%"}}>
-  <AssessmentCard title='Attempted' value='10'  />
-</div>
+              <button
+                type="button"
+                className="    text-gray-800 "
+                aria-label="Close"
+                onClick={closeModal}
+              >
+                <svg
+                  className="w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M18 6L6 18" />
+                  <path d="M6 6l12 12" />
+                </svg>
+              </button>
+            </div>
 
-<div  style={{width:"45%"}}>
-<AssessmentCard title='Correct / Incorrect' value='10' />
-</div>
+            <div className={`${text2} mt-1 text-left`}>
+              subtitle
+            </div>
 
-                {/* 
-               */}
-  </div>
+          </div>
 
-  <button
-              type="button"
-              className={`text-white ${logoBtnColor}  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-6 mb-4 `}
-             
-            >
-             View Detail Summary
-            </button>    
+          <div className="px-4 pb-4">
+
+            <div className='flex justify-between  gap-4  '>
+              <div style={{ width: "100%" }}>
+                <AssessmentCard title='Total' value='20' />
+              </div>
+
+              <div style={{ width: "100%" }}>
+                <AssessmentCard title='Attempted' value='10' />
+              </div>
+
+              <div style={{ width: "100%" }}>
+                <AssessmentCard title='Correct / Incorrect' value='9 / 10' />
+              </div>
+
+            </div>
+
+
+
+            <div className='flex gap-2 mt-4 mb-1 cursor-pointer'>
+              <div className={`${text2} `}>View Detail Summary
+
+              </div>
+
+              <svg
+                className="w-4 h-6 text-gray-800 dark:text-white "
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+
+            </div>
+
             <div className="flex items-center  ">
-  <div className={`${text2} flex-grow border-t`}  />
-  <span className={`${text1} mx-2`}>Or</span>
-  <div className={`${text2} flex-grow border-t`} />
-</div>
+              <div className={`${text2} flex-grow border-t`} />
+              <span className={`${text1} mx-4`}>OR</span>
+              <div className={`${text2} flex-grow border-t`} />
+            </div>
+
+            <div className={`${text2} mt-4`}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quibusdam architecto quae nam dolores id voluptate, voluptates, placeat doloribus perferendis vitae quisquam nisi unde beatae quidem animi molestiae molestias necessitatibus?
+            </div>
+
+            <div className="col-span-8 lg:col-span-6 relative flex items-center mt-4" id="div2">
+              <span className="absolute inset-y-0 left-3 flex items-center">
+                <FaSearch className="text-gray-400" />
+              </span>
+              <input
+                ref={searchInputRef}
+                type="text"
+                placeholder="Search..."
+                className="bg-gray-100 w-full pl-10 pr-10 h-9 border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+                onKeyDown={handleKeyDown} // Add onKeyDown event handler
+              />
+              <span className="absolute inset-y-0 right-3 flex items-center">
+                <FaMicrophone className="text-gray-400 cursor-pointer" onClick={handleMicClick} />
+              </span>
+            </div>
 
 
-<button
-        type="button"
-        className="border border-gray-500  bg-transparent cursor-pointer font-medium rounded-lg text-sm px-5 py-2.5 mt-4 mb-4 focus:outline-none hover:bg-gray-200 w-full"
-        style={{
-          border:"1px solid rgb(226, 226, 226)",
-          color:"rgb(107 114 128)"
-        }}
-      >
-       Take New Assessment      
-      </button>
- 
+            <button
+              type="button"
+              className={`text-white ${logoBtnColor} w-full  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-4     `}
 
-      <button
+            >
+              Start Practice
+            </button>
+
+            {/* <button
         type="button"
         className="border border-gray-500  bg-transparent cursor-pointer font-medium rounded-lg text-sm px-5 py-2.5  focus:outline-none hover:bg-gray-200 w-full"
         style={{
@@ -117,9 +209,9 @@ type SubmitPopupProps = {
         }}
       >
         Home     
-      </button>
-              </div>
-              {/* <div className="flex justify-end items-center gap-x-2 py-3 px-4 border-t">
+      </button> */}
+          </div>
+          {/* <div className="flex justify-end items-center gap-x-2 py-3 px-4 border-t">
                
                {
                 
@@ -158,9 +250,9 @@ type SubmitPopupProps = {
                 }
                
               </div> */}
-            </div>
-          </div>
         </div>
+      </div>
+    </div>
   );
 };
 
