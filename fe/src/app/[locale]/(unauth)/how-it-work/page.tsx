a6359253b39848eb0218bc3afae91d9dd3c3a512
vishal1145 <vishal.kumar1145@gmail.com>
2024-11-27 19:32:38 +0530

    .

--- fe/src/app/[locale]/(unauth)/how-it-work/page.tsx
+++ fe/src/app/[locale]/(unauth)/how-it-work/page.tsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import HowItWorks from './HowItWork'
-import { Metadata } from 'next'; // Import Metadata type if you need it for TypeScript
 
-export const metadata: Metadata = {
-  title: "How It Works",
-  description: "Learn how Preppal works and how you can create and practice customizable online papers.",
-  openGraph: {
-    title: "How It Works | Preppal",
-    description: "Learn how Preppal works and how you can create and practice customizable online papers.",
-    url: "https://preppal.club/how-it-works",
-  },
-  twitter: {
-    title: "How It Works",
-    description: "Learn how Preppal works and how you can create and practice customizable online papers.",
-  },
-};
 const page = () => {
   return (
+    <>
+    <head>
+      <meta property="og:title" content="How It Works" />
+    </head>
      <HowItWorks/>
+    </>
   )
 }
 
