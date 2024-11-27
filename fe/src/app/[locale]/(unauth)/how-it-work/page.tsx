0e339d7d12a8ab284c964ffcd02e9af6dd626c90
vishal1145 <vishal.kumar1145@gmail.com>
2024-11-27 20:07:40 +0530

    .

--- fe/src/app/[locale]/(unauth)/how-it-work/page.tsx
+++ fe/src/app/[locale]/(unauth)/how-it-work/page.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import HowItWorks from './HowItWork'
-import { NextSeo } from 'next-seo';
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: "How It Works | Preppal",
+  description: "Learn how Preppal works and how you can create and practice customizable online papers.",
+  openGraph: {
+    title: "How It Works | Preppal",
+    description: "Learn how Preppal works and how you can create and practice customizable online papers.",
+    url: "https://preppal.club/how-it-works",
+  },
+  twitter: {
+    title: "How It Works | Preppal",
+    description: "Learn how Preppal works and how you can create and practice customizable online papers.",
+  },
+};
 const page = () => {
   return (
     <>
-  <NextSeo
-        title="How It Works | Preppal"
-        description="Learn how Preppal works and how you can create and practice customizable online papers."
-        openGraph={{
-          title: "How It Works | Preppal",
-          description: "Learn how Preppal works and how you can create and practice customizable online papers.",
-          url: "https://preppal.club/how-it-works",
-        }}
-       
-      />
      <HowItWorks/>
     </>
   )
