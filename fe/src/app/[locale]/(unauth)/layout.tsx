27d1833e80472a128f64b2d1534ca73a3d211872
vishal1145 <vishal.kumar1145@gmail.com>
2024-12-03 17:44:51 +0530

    meta

--- fe/src/app/[locale]/(unauth)/layout.tsx
+++ fe/src/app/[locale]/(unauth)/layout.tsx
@@ -1,8 +1,3 @@
-// import Link from 'next/link';
-
-// import { LocaleSwitcher } from '@/components/LocaleSwitcher';
-// import { BaseTemplate } from '@/templates/BaseTemplate';
-// 'use client'
 import '../../../styles/app.css';
 import '@/styles/global.css';
 
@@ -15,34 +10,38 @@ import { unstable_setRequestLocale } from 'next-intl/server';
 import { AppConfig } from '@/utils/AppConfig';
 
 import DefaultPage from './default/page';
+export async function generateMetadata() {
+  const title = "Create and Practice Online Papers | Customizable Student Practice Tests";
+  const description =
+    "Empower students to create customizable online papers and practice tests by topic. Enhance learning with tailored questions and topics. Start practicing now!";
+  const keywords =
+    "Online paper creation, student practice tests, customizable practice papers, study aid for students, online quiz creation, subject-based practice papers, topic-specific tests, student learning platform, practice exams for students";
+  const ogImage = "/favicon3.ico";
+  const url = "https://preppal.club";
 
-export const metadata: Metadata = {
-  title: 'Paper Text | Create and practice online papers',
-  description: 'Create and Practice Online Papers | Customizable Student Practice Tests',
+  return {
+    title,
+    description,
+    keywords,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: ogImage,
+          alt: "Customizable Student Practice Tests",
+        },
+      ],
+      url,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+    },
+  };
+}
 
-  icons: [
-    {
-      rel: 'apple-touch-icon',
-      url: '/apple-touch-icon.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      url: '/favicon-32x32.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      url: '/favicon-16x16.png',
-    },
-    {
-      rel: 'icon',
-      url: '/favicon.ico',
-    },
-  ],
-};
 
 export function generateStaticParams() {
   return AppConfig.locales.map(locale => ({ locale }));
