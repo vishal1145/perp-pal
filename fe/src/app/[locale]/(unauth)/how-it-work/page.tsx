080a48db2c765137c2dfdd154be09bf39beb328b
vishal1145 <vishal.kumar1145@gmail.com>
2024-12-03 15:43:11 +0530

    metatag

--- fe/src/app/[locale]/(unauth)/how-it-work/page.tsx
+++ fe/src/app/[locale]/(unauth)/how-it-work/page.tsx
@@ -1,26 +1,34 @@
 import React from 'react'
 import HowItWorks from './HowItWork'
-import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: "How It Works | Preppal",
-  description: "Learn how Preppal works and how you can create and practice customizable online papers.",
-  openGraph: {
-    title: "How It Works | Preppal",
-    description: "Learn how Preppal works and how you can create and practice customizable online papers.",
-    url: "https://preppal.club/how-it-works",
-  },
-  twitter: {
-    title: "How It Works | Preppal",
-    description: "Learn how Preppal works and how you can create and practice customizable online papers.",
-  },
-};
+export async function generateMetadata({ params: { text } }: {
+  params: { text: string };
+}) {
+  const pageTitle = `How It Works | Preppal`;
+  const pageDescription = `Learn how Preppal works and how you can create and practice customizable online papers.`;
+  const url = "https://preppal.club/how-it-works";
+  return {
+    title: pageTitle,
+    description: pageDescription,
+    openGraph: {
+      title: pageTitle,
+      description: pageDescription,
+      url: url,
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: pageTitle,
+      description: pageDescription,
+      url: url,
+    },
+  };
+}
+
+ 
 
 const page = () => {
   return (
-    <>
      <HowItWorks/>
-    </>
   )
 }
 
