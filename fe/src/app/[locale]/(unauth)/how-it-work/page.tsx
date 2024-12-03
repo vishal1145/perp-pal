f720ccb75e7b907265d1005ff03c95890efc5efc
vishal1145 <vishal.kumar1145@gmail.com>
2024-12-03 16:27:40 +0530

    meta

--- fe/src/app/[locale]/(unauth)/how-it-work/page.tsx
+++ fe/src/app/[locale]/(unauth)/how-it-work/page.tsx
@@ -2,27 +2,27 @@ import React from 'react';
 import HowItWorks from './HowItWork';
 import { Metadata } from 'next';
 
-export async function generateMetadata() {
-  const pageTitle = `How It Works | Preppal`;
-  const pageDescription = `Learn how Preppal works and how you can create and practice customizable online papers.`;
-  const url = "https://preppal.club/how-it-works";
+// export async function generateMetadata() {
+//   const pageTitle = `How It Works | Preppal`;
+//   const pageDescription = `Learn how Preppal works and how you can create and practice customizable online papers.`;
+//   const url = "https://preppal.club/how-it-works";
 
-  return {
-    title: pageTitle,
-    description: pageDescription,
-    openGraph: {
-      title: pageTitle,
-      description: pageDescription,
-      url: url,
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: pageTitle,
-      description: pageDescription,
-      url: url,
-    },
-  };
-}
+//   return {
+//     title: pageTitle,
+//     description: pageDescription,
+//     openGraph: {
+//       title: pageTitle,
+//       description: pageDescription,
+//       url: url,
+//     },
+//     twitter: {
+//       card: 'summary_large_image',
+//       title: pageTitle,
+//       description: pageDescription,
+//       url: url,
+//     },
+//   };
+// }
 
 export const metadata: Metadata = {
   title: "Privacy Policy",
