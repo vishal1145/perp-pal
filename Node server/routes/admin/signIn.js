cc73593ba68218443f56cd6dc37fe5350eb9fc23
vishal1145 <vishal.kumar1145@gmail.com>
2025-01-30 12:37:22 +0530

    step1

--- Node server/routes/admin/signIn.js
+++ Node server/routes/admin/signIn.js
@@ -7,47 +7,47 @@ const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/', async (req, res) => {
-    const { email, password } = req.body;
+    // const { email, password } = req.body;
 
     try {
         // Find the admin in the database
-        const user = await Admin.findOne({ email });
-        if (!user) {
-            return res.status(400).json({ error: 'Admin not found!' });
-        }
+        // const user = await Admin.findOne({ email });
+        // if (!user) {
+        //     return res.status(400).json({ error: 'Admin not found!' });
+        // }
 
-        // Check if the password is valid
-        const validPassword = await bcryptjs.compare(password, user.password);
-        if (!validPassword) {
-            return res.status(400).json({ error: 'Invalid credentials!' });
-        }
+        // // Check if the password is valid
+        // const validPassword = await bcryptjs.compare(password, user.password);
+        // if (!validPassword) {
+        //     return res.status(400).json({ error: 'Invalid credentials!' });
+        // }
 
-        // Generate a JWT token
-        const tokenData = {
-            id: user._id,
-            email: user.email
-        };
-        const token = jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: '7d' });
+        // // Generate a JWT token
+        // const tokenData = {
+        //     id: user._id,
+        //     email: user.email
+        // };
+        // const token = jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: '7d' });
 
 
-        // Set the token in a cookie
-        const isProduction = process.env.NODE_ENV === 'production';
+        // // Set the token in a cookie
+        // const isProduction = process.env.NODE_ENV === 'production';
 
-        // Set the token in an HTTP-only cookie
-        res.cookie('token', token, {
-            httpOnly: true,  // Prevents JavaScript access (XSS protection)
-            secure: isProduction, // Secure in production, non-secure in localhost
-            sameSite: isProduction ? 'None' : 'Lax', // CSRF protection
-            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiration
-        });
+        // // Set the token in an HTTP-only cookie
+        // res.cookie('token', token, {
+        //     httpOnly: true,  // Prevents JavaScript access (XSS protection)
+        //     secure: isProduction, // Secure in production, non-secure in localhost
+        //     sameSite: isProduction ? 'None' : 'Lax', // CSRF protection
+        //     maxAge: 7 * 24 * 60 * 60 * 1000, 
+        // });
 
 
         // Respond with success and user data
         return res.status(200).json({
             message: 'Login successful',
             success: true,
-            data: user,
-            token: token
+            // data: user,
+            // token: token
         });
     } catch (error) {
         console.error(error);
