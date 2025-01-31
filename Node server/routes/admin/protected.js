e42d5305026d1e4705a9dda42b40e7265ba36842
vishal1145 <vishal.kumar1145@gmail.com>
2025-01-31 10:50:40 +0530

    commit

--- Node server/routes/admin/protected.js
+++ Node server/routes/admin/protected.js
@@ -2,7 +2,6 @@ const express = require("express");
 const getDataFromToken = require("../middleware/getDataFRomToken");
 const router = express.Router();
 
-// Protected route to get user data from token
 router.get("/", getDataFromToken, (req, res) => {
     return res.status(200).json({
         success: true,
